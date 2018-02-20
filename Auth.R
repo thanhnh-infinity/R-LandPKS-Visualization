fd588a9f0fac8b84075bd81d8d46c79b9cdfcf4f
thanhnh-infinity <thanhnh2911@gmail.com>
2018-02-20 13:59:17 -0700

    first upload

---
+++ Auth.R
@@ -0,0 +1,3 @@
+#This file contains client_key and secret and should not be uploaded to a public repository
+myKey = "410858290704-pjirleeo4m55hme1ammq00fsbeb8nk33.apps.googleusercontent.com"
+mySecret = "V75kZtpAn-AEUnY0BZs6TFBN"
